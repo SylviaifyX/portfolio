8bd6c25135935adf97b7fa961e0dd341852f1666
Christian Ezeanyaeche <christianezeanyaeche@gmail.com>
2023-09-27 10:13:40 +0100

    fix: beautify

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+const menu = document.querySelector("header nav ul");
+const icon = document.querySelector(".hamburger-icon");
+
+function toggleMenu(e) {
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
-ScrollReveal({ reset: true,
- distance:'80px',
- duration:2000,
- delay:200
+icon.addEventListener("click", toggleMenu);
 
-});
+ScrollReveal({ reset: true, distance: "80px", duration: 2000, delay: 200 });
 
-// ScrollReveal().reveal('.section__pic-container', { origin: 'top' });
-ScrollReveal().reveal('.section__text__p1', { origin: 'left' });
-ScrollReveal().reveal('.title', { origin: 'right' });
+ScrollReveal().reveal(".section__text__p1", { origin: "left" });
+ScrollReveal().reveal(".title", { origin: "right" });
